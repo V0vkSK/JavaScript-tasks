d7503c961e5c97a6b25962f626314b76f7e9e22c
Volodymyr Kyrylenko <vs.k@me.com>
2022-08-11 16:48:45 +0300

    ver2

--- js/task-08.js
+++ js/task-08.js
@@ -3,16 +3,13 @@
 import users from "./users.js";
 
 const getUsersWithFriend = (users, friendName) => {
-    let equiFriend = [];
+    
     const allFriends = users.reduce(function (accFriends, user) {
-        accFriends.push(...user.friends);
-        
-        
-        equiFriend.push(user.name);   
-    console.log( 
-            equiFriend);
-        
-        return accFriends, equiFriend;
+
+        if (user.friends.includes(friendName)) {
+                accFriends.push(user.name);
+            }  
+        return accFriends;
     }, [])
         console.log( 
            'allFriends     ', allFriends);
